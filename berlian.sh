695a8c1bfb2d9850f294c8e2887e9d7a3795c892
titis69 <117496052+titis69@users.noreply.github.com>
2024-10-25 03:31:37 +0700

    Update berlian.sh

--- berlian.sh
+++ berlian.sh
@@ -400,7 +400,7 @@ wget -O /etc/haproxy/haproxy.cfg "${REPO}cfgx/haproxy.cfg" >/dev/null 2>&1
 wget -O /etc/nginx/conf.d/xray.conf "${REPO}cfgx/xray.conf" >/dev/null 2>&1
 sed -i "s/xxx/${domain}/g" /etc/haproxy/haproxy.cfg
 sed -i "s/xxx/${domain}/g" /etc/nginx/conf.d/xray.conf
-curl ${REPO}Cfg/nginx.conf > /etc/nginx/nginx.conf
+curl ${REPO}cfgx/nginx.conf > /etc/nginx/nginx.conf
 cat /etc/xray/xray.crt /etc/xray/xray.key | tee /etc/haproxy/hap.pem
 chmod +x /etc/systemd/system/runn.service
 rm -rf /etc/systemd/system/xray.service.d
