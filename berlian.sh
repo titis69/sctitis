9e762711e2c7848493c8a4f7c8a3ed783d6c15bc
titis69 <117496052+titis69@users.noreply.github.com>
2025-10-12 19:50:27 +0700

    Update berlian.sh

--- berlian.sh
+++ berlian.sh
@@ -382,17 +382,17 @@ echo "& plughin Account" >>/etc/ssh/.ssh.db
 }
 function install_xray() {
 clear
-print_install "Core Xray 1.8.17 Latest Version"
+print_install "Core Xray 24.9.30 Latest Version"
 domainSock_dir="/run/xray";! [ -d $domainSock_dir ] && mkdir  $domainSock_dir
 chown www-data.www-data $domainSock_dir
 #latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
-#bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --force --version 24.9.30 
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.8.17
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --u www-data --version 24.9.30 
+#bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.8.17
 wget -O /etc/xray/config.json "${REPO}cfgx/config.json" >/dev/null 2>&1
 wget -O /etc/systemd/system/runn.service "${REPO}cfgx/runn.service" >/dev/null 2>&1
 domain=$(cat /etc/xray/domain)
 IPVS=$(cat /etc/xray/ipvps)
-print_success "Core Xray 1.8.17 Latest Version"
+print_success "Core Xray 24.9.30 Latest Version"
 clear
 curl -s ipinfo.io/city >>/etc/xray/city
 curl -s ipinfo.io/org | cut -d " " -f 2-10 >>/etc/xray/isp
